2dfca50dec46b68906cacecc911081a7de4e2f49
haruki7049 <tontonkirikiri@gmail.com>
2024-06-05 10:30:48 +0900

    chore: first commit

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs { inherit self; system = "x86_64-linux"; };
+    in
+    {
+      devShells."x86_64-linux".default = pkgs.mkShell {
+        packages = with pkgs; [
+          bun
+        ];
+      };
+    };
+}
